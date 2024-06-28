619a778f8b656db41a76339aea02e796a29fc448
bobbygour30 <bobby30gour@gmail.com>
2024-06-28 17:12:46 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Footer() {
+  return (
+    <section className="relative overflow-hidden py-10 bg-sky-400 h-[400px]  mt-[300px]">
+      <div className="relative z-10 mx-auto max-w-7xl px-4">
+        <div className="-m-6 flex flex-wrap">
+          <div className="w-full p-6 md:w-1/2 lg:w-5/12">
+            <div className="flex h-full flex-col justify-between">
+              <div className="mb-4  items-center">
+                <p className="text-2xl font-bold">Instructions:</p>
+                <div>
+                <ul className="mt-8 text-xl flex flex-wrap " >
+                    <li>1. Signup with your Google account.</li>
+                    <li>2. Create posts and submit it.</li>
+                    <li>3. You can write something along with a picture.</li>
+                    <li>4. The data will be saved in your Google account.</li>
+                </ul>
+                </div>
+              </div>
+              <div>
+                <p className="w-full text-sm cursor-pointer ml-[350px] duration-300 mt-16 text-gray-600 hover:text-lg">
+                  &copy; Copyright 2023. All Rights Reserved by Bobby.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+                Company
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Features
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Pricing
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Affiliate Program
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Press Kit
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+                Support
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Account
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Help
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Customer Support
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-3/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+                Legals
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Licensing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className='mt-1 text-xl duration-300 font-bold  rounded-md hover:text-2xl'>BLOG APP</div>
+   
+  )
+}
+
+export default Logo
